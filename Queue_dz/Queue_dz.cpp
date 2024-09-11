fe44f3403f576a79d00d7e83a2208e4993844ec5
Ivan <ivansamiko1996@gmail.com>
2024-09-11 20:19:45 +0300

    Добавьте файлы проекта.

---
+++ Queue_dz/Queue_dz.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <chrono>
+
+
+using namespace std;
+using namespace std::chrono;
+
+struct Client{
+
+    string name;
+    int priority;
+    system_clock::time_point submitted_at;
+public:
+
+};
+
+int main()
+{
+    
+   
+}
+
